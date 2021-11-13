0f7b0701834dd5d63f05b220988da30210a86980
LEE <dldnsgud12@naver.com>
2021-11-13 20:57:12 +0900

    #7.1 Creating Account part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,18 @@
+import User from "../models/User";
+
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Create Account" });
 };
-export const postJoin = (req, res) => {
-  return res.render("/");
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
 };
 export const remove = (req, res) => res.send("remove");
 export const edit = (req, res) => res.send("edit");
