ea24e474410fd05a99c037986293e13ec5a87a00
LEE <dldnsgud12@naver.com>
2021-11-23 21:46:13 +0900

    #7.6 Login part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 import User from "../models/User";
-
+import bcrypt from "bcrypt";
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Create Account" });
 };
@@ -7,7 +7,7 @@ export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
   // password 확인
   if (password !== password2) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle: "Create Account",
       errorMessage: "Password confirmation does not match",
     });
@@ -39,21 +39,30 @@ export const postJoin = async (req, res) => {
   }
 };
 
+//Login
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
-
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
-  const exists = await User.exists({ username });
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
   //check if account exist
-  if (!exists) {
+  if (!user) {
     return res.status(400).render("login", {
-      pageTitle: "Login",
+      pageTitle,
       errorMessage: "An account whit this username does not exists",
     });
   }
   //check if password correct
-  res.redirect("/");
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
+    });
+  }
+
+  return res.redirect("/");
 };
 
 export const remove = (req, res) => res.send("remove");
