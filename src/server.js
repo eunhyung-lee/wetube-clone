2917ddc95716a00c5ea9e7344f909a0b5a9f03d7
LEE <dldnsgud12@naver.com>
2021-11-24 21:42:28 +0900

    #7.8 Sessions and Cookies

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import morgan from "morgan"; //morgan 불러오기
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import session from "express-session";
 
 const app = express(); //create express application
 const logger = morgan("dev");
@@ -11,6 +12,20 @@ app.set("view engine", "pug"); // view engine을 pug로 설정
 app.set("views", process.cwd() + "/src/views");
 app.use(logger); //log
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id}\n${req.session.potato}`);
+});
+
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
